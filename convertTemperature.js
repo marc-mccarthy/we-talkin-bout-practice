fb03eb81e6f6e519a4e443eec37c0bc53474fe82
Marc McCarthy <marstheory20@gmail.com>
2023-01-29 11:35:56 -0600

    added a prime coach leetcode solution.

--- convertTemperature.js
+++ convertTemperature.js
@@ -1,6 +1,9 @@
-var convertTemperature = function (celsius) {
+// Convert celsius to kelvin and fahrenheit
+let convertTemperature = function (celsius) {
 	let ans = [];
 	ans.push(celsius + 273.15);
 	ans.push(celsius * 1.8 + 32.0);
 	return ans;
 };
+
+console.log(convertTemperature(246.75));
