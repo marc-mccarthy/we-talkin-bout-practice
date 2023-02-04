b4c0e6aaf4f069f8e8ea7d8dd4425b16dca2dc93
Marc McCarthy <marstheory20@gmail.com>
2023-02-04 14:56:47 -0600

    added problem solution.

---
+++ bestTimeBuyStock.js
@@ -0,0 +1,14 @@
+let maxProfit = function (prices) {
+	let min = prices[0];
+	let max = 0;
+	for (let i = 1; i < prices.length; i++) {
+		if (prices[i] < min) {
+			min = prices[i];
+		} else if (prices[i] - min > max) {
+			max = prices[i] - min;
+		}
+	}
+	return max;
+};
+
+console.log(maxProfit([2, 3, 2, 5, 7, 10]));
