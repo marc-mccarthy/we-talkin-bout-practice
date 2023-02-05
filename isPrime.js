f8ba4e00c97a2794146e7a6a602b620683706632
Marc McCarthy <marstheory20@gmail.com>
2023-02-05 13:43:41 -0600

    Added isPrime problem.

---
+++ isPrime.js
@@ -0,0 +1,16 @@
+let isPrime = (num) => {
+	if (num <= 1) {
+		return "Is not a prime number";
+	}
+	let c = 2;
+	while (c * c <= num) {
+		if (num % c === 0) {
+			return "Is not a prime number";
+		} else {
+			c += 1;
+		}
+	}
+	return "Is a prime number";
+};
+
+console.log(isPrime(5));
